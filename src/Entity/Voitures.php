1227a582e19dcefc70af0ae6c6726a96cfc3d74f
MaurineDuray <maurine.duray@skynet.be>
2022-10-24 09:26:48 +0200

    first commit

---
+++ src/Entity/Voitures.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoituresRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoituresRepository::class)]
+class Voitures
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $marque = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $modele = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column]
+    private ?int $km = null;
+
+    #[ORM\Column]
+    private ?int $prix = null;
+
+    #[ORM\Column]
+    private ?int $nbProprio = null;
+
+    #[ORM\Column]
+    private ?int $cylindree = null;
+
+    #[ORM\Column]
+    private ?int $puissance = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $carburant = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $annee = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $transmission = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $options = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): self
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): self
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getKm(): ?int
+    {
+        return $this->km;
+    }
+
+    public function setKm(int $km): self
+    {
+        $this->km = $km;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getNbProprio(): ?int
+    {
+        return $this->nbProprio;
+    }
+
+    public function setNbProprio(int $nbProprio): self
+    {
+        $this->nbProprio = $nbProprio;
+
+        return $this;
+    }
+
+    public function getCylindree(): ?int
+    {
+        return $this->cylindree;
+    }
+
+    public function setCylindree(int $cylindree): self
+    {
+        $this->cylindree = $cylindree;
+
+        return $this;
+    }
+
+    public function getPuissance(): ?int
+    {
+        return $this->puissance;
+    }
+
+    public function setPuissance(int $puissance): self
+    {
+        $this->puissance = $puissance;
+
+        return $this;
+    }
+
+    public function getCarburant(): ?string
+    {
+        return $this->carburant;
+    }
+
+    public function setCarburant(string $carburant): self
+    {
+        $this->carburant = $carburant;
+
+        return $this;
+    }
+
+    public function getAnnee(): ?\DateTimeInterface
+    {
+        return $this->annee;
+    }
+
+    public function setAnnee(\DateTimeInterface $annee): self
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    public function getTransmission(): ?string
+    {
+        return $this->transmission;
+    }
+
+    public function setTransmission(string $transmission): self
+    {
+        $this->transmission = $transmission;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getOptions(): ?string
+    {
+        return $this->options;
+    }
+
+    public function setOptions(string $options): self
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+}
